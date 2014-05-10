0d1015617498e88640f5b6cec89fe9964e6bcff3
Akira345 <akira345@gmail.com>
2014-05-10 23:24:27 +0900

    モデルファイルを修正し、制約を追加。

--- db/migrate/20140510142203_create_cas.rb
+++ db/migrate/20140510142203_create_cas.rb
@@ -1,17 +1,15 @@
 class CreateCas < ActiveRecord::Migration
   def change
     create_table :cas do |t|
-      t.integer :user_id
-      t.string :ca_password
-      t.string :ca_param
-      t.string :hostname
-      t.string :domain_name
-      t.string :dn_c
-      t.string :dn_st
-      t.string :dn_l
-      t.string :dn_o
+      t.integer :user_id 
+      t.string :ca_password, :limit=>16, :null=>false
+      t.string :hostname, :null=>false
+      t.string :domain_name, :null=>false
+      t.string :dn_c, :null=>false
+      t.string :dn_st, :null=>false
+      t.string :dn_l, :null=>false
+      t.string :dn_o, :null=>false
       t.string :dn_ou
-
       t.timestamps
     end
   end
